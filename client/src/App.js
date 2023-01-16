37526f9975ad78b68c5fdcb347095b32e4093aa2
hhundley <65366145+hhundley@users.noreply.github.com>
2023-01-16 11:32:38 -0600

    adding login functionality

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,43 @@
 import React,{useState} from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from "./components/header";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 import Navigation from "./components/navbar";
 import Main from "./components/main";
 import About from "./components/about";
 import Policies from "./components/policies";
-import Contact from "./components/contact"
+import Contact from "./components/contact";
+import Login from "./components/login";
 import Footer from "./components/footer";
+
+// Construct our main GraphQL API endpoint
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem('id_token');
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  // Set up our client to execute the `authLink` middleware prior to making the request to our GraphQL API
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 function App() {
   const [pageState,setPageState] = useState({
@@ -13,8 +45,11 @@ function App() {
     about:false,
     policies:false,
     contact:false,
+    login:false,
+    cart:false,
    })
   return (
+    <ApolloProvider client={client}>
     <div>
       <Header />
       <Navigation pageState={pageState} setPageState={setPageState} />
@@ -22,8 +57,11 @@ function App() {
       {pageState.about? <About />: ''}
       {pageState.policies? <Policies />: ''}
       {pageState.contact? <Contact />: ''}
+      {pageState.login? <Login />: ''}
+      {/* {pageState.cart? <Cart />: ''} */}
       <Footer />
     </div>
+    </ApolloProvider>
   );
 }
 
--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -1,22 +1,32 @@
 import React from 'react';
 import Nav from 'react-bootstrap/esm/Nav';
 import Navbar from 'react-bootstrap/esm/Navbar';
+import avatar from '../../src/assets/person-circle.svg';
+import cart from '../../src/assets/cart.svg';
 
 function Navigation({pageState,setPageState}) {
     const handleHome = () => {
-        setPageState({...pageState,about:false,main:true,policies:false,contact:false})
+        setPageState({...pageState,about:false,main:true,policies:false,contact:false,login:false})
     }
 
     const handleAbout = () => {
-        setPageState({...pageState,about:true,main:false,policies:false,contact:false})
+        setPageState({...pageState,about:true,main:false,policies:false,contact:false,login:false})
     }
 
     const handlePolicies = () => {
-        setPageState({...pageState,about:false,main:false,policies:true,contact:false})
+        setPageState({...pageState,about:false,main:false,policies:true,contact:false,login:false})
     }
 
     const handleContact = () => {
-        setPageState({...pageState,about:false,main:false,policies:false,contact:true})
+        setPageState({...pageState,about:false,main:false,policies:false,contact:true,login:false})
+    }
+
+    const handleLogin = () => {
+        setPageState({...pageState,about:false,main:false,policies:false,contact:false,login:true})
+    }
+
+    const handleCart = () => {
+        setPageState({...pageState,about:false,main:false,policies:false,contact:false,login:false,cart:true})
     }
 
     return (
@@ -28,6 +38,8 @@ function Navigation({pageState,setPageState}) {
                 <Nav.Link href="#" onClick={handleAbout}>About</Nav.Link>
                 <Nav.Link href="#" onClick={handlePolicies}>Policies</Nav.Link>
                 <Nav.Link href="#" onClick={handleContact}>Contact</Nav.Link>
+                <Nav.Link href="#" onClick={handleLogin}><img src={avatar} id="avatar" alt="avatar" /></Nav.Link>
+                <Nav.Link href="#" onClick={handleCart}><img src={cart} id="cart" alt="cart" /></Nav.Link>
             </Nav>
             </Navbar.Collapse>
         </Navbar>
